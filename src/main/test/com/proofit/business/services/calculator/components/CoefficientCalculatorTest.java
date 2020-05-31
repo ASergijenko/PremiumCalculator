342e594ce954cbee3e2f75df6fab16b6cfb32fa9
Aleksandrs Sergijenko <a.sergijenko6415@gmail.com>
2020-05-31 22:28:25 +0300

    Premium Calculator V1.0

--- src/main/test/com/proofit/business/services/calculator/components/CoefficientCalculatorTest.java
+++ src/main/test/com/proofit/business/services/calculator/components/CoefficientCalculatorTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.math.BigDecimal;
 
 import static org.junit.Assert.assertEquals;
+
 public class CoefficientCalculatorTest {
 
     private CoefficientCalculator victim;
